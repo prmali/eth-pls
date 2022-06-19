9f196687fcde7284b423e8335327302c3c2f46b7
prathik <37804760+prmali@users.noreply.github.com>
2022-06-19 07:45:27 +0530

    Modify ENS name discovery

--- enspls.py
+++ enspls.py
@@ -3,8 +3,12 @@ import certifi
 import ssl
 import aiohttp
 import asyncio
-import requests
-import json
+import secrets
+from decouple import config
+from web3 import Web3
+from ens import ENS
+
+from data import names
 
 SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
 
@@ -24,67 +28,21 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-async def count(session, name, retry_limit):
+async def get(ns, session, name, retry_limit):
     v2 = None
     for attempt in range(retry_limit):
         try:
-            q1 = """
-            query($name: String!) {
-                domains(where: { name: $name }) {
-                    labelhash
-                }
-            }
-            """
+            owner = ns.owner(name)
 
-            q2 = """
-            query($id: ID!) {
-                registration(id: $id) {
-                    id
-                    domain {
-                        name
-                        __typename
-                    }
-                    registrant {
-                        id
-                        __typename
-                    }
-                    __typename
-                }
-            }
-            """
+            if (owner == "0x0000000000000000000000000000000000000000"):
+                valids.append(name)
+                print(
+                    f'{bcolors.HEADER}{name}{bcolors.ENDC} {bcolors.OKGREEN}VALID{bcolors.ENDC}')
+            else:
+                print(
+                    f'{bcolors.HEADER}{name}{bcolors.ENDC} {bcolors.FAIL}BRUH{bcolors.ENDC}')
 
-            v1 = {
-                'name': name
-            }
-
-            if not v2:
-                async with session.post('https://api.thegraph.com/subgraphs/name/ensdomains/ens', json={'query': q1, 'variables': v1}, ssl=SSL_CONTEXT) as res1:
-                    r1 = await res1.json()
-                    #print(name, json.dumps(r1, indent=2))
-
-                    if len(r1['data']['domains']) == 0:
-                        print(
-                            f'{bcolors.HEADER}{name}{bcolors.ENDC} {bcolors.WARNING}T E S T{bcolors.ENDC}')
-                        tests.append(name)
-                        return
-
-                    v2 = {
-                        'id': r1['data']['domains'][0]['labelhash']
-                    }
-
-            async with session.post('https://api.thegraph.com/subgraphs/name/ensdomains/ens', json={'query': q2, 'variables': v2}, ssl=SSL_CONTEXT) as res2:
-                r2 = await res2.json()
-                #print(name, json.dumps(r2, indent=2))
-
-                if not r2['data']['registration']:
-                    valids.append(name)
-                    print(
-                        f'{bcolors.HEADER}{name}{bcolors.ENDC} {bcolors.OKGREEN}V A L I D{bcolors.ENDC}')
-                else:
-                    print(
-                        f'{bcolors.HEADER}{name}{bcolors.ENDC} {bcolors.FAIL}B R U H{bcolors.ENDC}')
-
-                return
+            return
         except Exception as e:
             pass
 
@@ -93,17 +51,21 @@ async def count(session, name, retry_limit):
 
 async def main(retry_limit=500):
     start_time = time.time()
+    ALCHEMY_KEY = config('ALCHEMY_KEY')
+    w3 = Web3(Web3.HTTPProvider(
+        f'https://eth-mainnet.alchemyapi.io/v2/{ALCHEMY_KEY}'))
+    ns = ENS.fromWeb3(w3)
+
     async with aiohttp.ClientSession(trust_env=True) as session:
-        print("--- 3 FILL COUNT ---")
-        await asyncio.gather(*[count(session, f'{str(num).zfill(3)}.eth', retry_limit) for num in range(1, 999)])
-        print("--- 4 FILL COUNT ---")
-        await asyncio.gather(*[count(session, f'{str(num).zfill(4)}.eth', retry_limit) for num in range(1, 9999)])
+        await asyncio.gather(*[get(ns, session, f'{name}.eth', retry_limit) for name in names])
+        # await asyncio.gather(*[get(ns, session, f'{str(num).zfill(3)}.eth', retry_limit) for num in range(644, 999)])
+        # await asyncio.gather(*[get(ns, session, f'{str(num).zfill(4)}.eth', retry_limit) for num in range(1, 9999)])
+        # await asyncio.gather(*[get(ns, session, f'{str(num).zfill(5)}.eth', retry_limit) for num in range(1, 99999)])
 
     finalized_time = time.time() - start_time
     print("--- DONE ---")
     print("--- %s seconds ---" % (finalized_time))
 
-    print('tests:', tests)
     print('valids:', valids)
 
 
